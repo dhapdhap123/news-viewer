5f025b0db5e1ff09796e1bfeaf564dcc40e16b84
dhapdhap123 <dhapdhap123@naver.com>
2022-10-06 23:52:13 +0900

    1006

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { React, useState } from "react";
+function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=318b5a093a2d4218b991bfcf0ae05515"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
